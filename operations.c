936c04e71dfa8730c5e974ade880ad2c466afdee
kauanatomb <kauanatomb@gmail.com>
2025-06-17 16:04:25 +0200

    improve main and start sorting operation functions

--- operations.c
+++ operations.c
@@ -1,33 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   operations.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ktombola <ktombola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/11 10:32:28 by ktombola          #+#    #+#             */
-/*   Updated: 2025/06/11 10:32:32 by ktombola         ###   ########.fr       */
+/*   Created: 2025/06/11 10:39:58 by ktombola          #+#    #+#             */
+/*   Updated: 2025/06/11 10:40:02 by ktombola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char *argv[])
-{
-    t_stack *a;
-    t_stack *b;
-
-    if (argc < 2)
-        return (ft_putstr_fd("Error args\n", 2), 0);
-    a = init_stack();
-    b = init_stack();
-    if (parse_args_into_stack(a, argc, argv) != 0)
-    {
-        ft_putstr_fd("Error parsing\n", 2);
-        free_stack(a);
-        return (1);
-    }
-    print_stack(a);
-    free_stack(a);
-    return (0);
-}
