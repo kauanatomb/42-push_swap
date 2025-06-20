16ed1c72967e1ed9229bc51994e2a18e35614230
kauanatomb <kauanatomb@gmail.com>
2025-06-20 15:58:07 +0200

    added sort 4 and update indifces

--- push_swap_utils2.c
+++ push_swap_utils2.c
@@ -14,15 +14,14 @@
 
 void	push_swap(t_stack *a, t_stack *b)
 {
-    (void)b;
 	if (a->size == 2)
 		sort_2(a);
 	else if (a->size == 3)
 		sort_3(a);
-	// else if (a->size == 4)
-	// 	sort_4(a, b);
-	// else if (a->size == 5)
-	// 	sort_5(a, b);
+	else if (a->size == 4)
+		sort_4(a, b);
+	else if (a->size == 5)
+		sort_5(a, b);
 	// else
 	// 	sort_big(a, b); // etapa futura
 }
@@ -55,4 +54,46 @@ void	sort_3(t_stack *a)
 	}
 	else if (x > y && y < z && x > z)
 		op_ra(a);
+}
+
+void	sort_4(t_stack *a, t_stack *b)
+{
+	t_node	*min;
+	t_node	*tmp;
+	int		idx;
+
+	min = a->head;
+	tmp = a->head;
+	idx = min->index;
+	while (tmp)
+	{
+		if (min->value > tmp->value)
+		{
+			min = tmp;
+			idx = tmp->index;
+		}
+		tmp = tmp->next;
+	}
+	while (a->head->value != min->value)
+	{
+		if (idx < a->size - idx)
+			op_ra(a);
+		else
+			op_rra(a);
+	}
+	op_pb(a, b);
+	sort_3(a);
+	op_pa(a, b);
+}
+
+void update_stack_indices(t_stack *s)
+{
+    t_node *current = s->head;
+    int idx = 0;
+
+    while (current)
+    {
+        current->index = idx++;
+        current = current->next;
+    }
 }
